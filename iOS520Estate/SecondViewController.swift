82331b8211ca8d7261bde23d22181e4e7faad172
Yonglun Liu <yonglunliu@YonglundeMac-mini.local>
2017-08-27 20:00:44 +0800

    Initial Commit

---
+++ iOS520Estate/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOS520Estate
+//
+//  Created by Yonglun Liu on 2017/8/27.
+//  Copyright © 2017年 YonglunTech. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
